00f93ec24cbdb84ac475e21338e984db0aa84ef9
Valmir Jr <valmirmassoni@gmail.com>
2018-03-15 15:52:29 -0300

    Starting WWDC scholarship project

---
+++ Metamorphosis.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: A SpriteKit based Playground
+
+import PlaygroundSupport
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var threeNode: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    func touchDown(atPoint pos : CGPoint) {
+
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
+
+// Load the SKScene from 'GameScene.sks'
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+if let scene = GameScene(fileNamed: "GameScene") {
+    // Set the scale mode to scale to fit the window
+    scene.scaleMode = .aspectFill
+    
+    // Present the scene
+    sceneView.presentScene(scene)
+}
+
+PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
---
+++ Metamorphosis.playground/Sources/Kindness.swift
@@ -0,0 +1,16 @@
+import Foundation
+import SpriteKit
+import UIKit
+
+class Kindness {
+    
+    var quotes: [String] = [
+        "Kindness Matters",
+        "Try Kindness",
+        "Be kind",
+        "Kindness inpires kindess"
+    ]
+    
+
+    
+}
